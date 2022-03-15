5b76dc3eaa4645fa6d2be68f5a7b3e2ae38e521a
Eugenia Brizuela <euge_brizuela@hotmail.com>
2022-03-15 10:20:43 -0300

    Modificaciones en repaso-django

--- index/views.py
+++ index/views.py
@@ -1,22 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
 
-# Create your views here.
+from django.shortcuts import render
 
 def index (request) :
-    return HttpResponse ('<h1>Bienvenidos a mi página de django</h1>')
+    return render (request, 'index.html', {})
 
 
 def plantilla (request) :
-    
-    template = loader.get_template ('plantilla.html')
     
     datos = {
         'lista': ['primero', 'segundo', 'tercero'],
         'nombre': 'Juancho',
     }
-    
-    plantilla_generada = template.render (datos)
-    
-    return HttpResponse (plantilla_generada)
+     
+    return render (request, 'plantilla.html', datos)
